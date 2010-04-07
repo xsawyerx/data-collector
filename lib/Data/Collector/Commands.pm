b68589ae2df9c379a87e679c8111433241ffe2d7
Sawyer X <xsawyerx@cpan.org>
2010-04-07 17:00:10 +0300

    adding ifconfig

--- lib/Data/Collector/Commands.pm
+++ lib/Data/Collector/Commands.pm
@@ -8,10 +8,11 @@ has 'commands' => (
     isa     => 'HashRef[Str]',
     traits  => ['Hash'],
     default => sub { {
-        cat  => '/bin/cat',
-        test => '/usr/bin/test',
-        echo => '/bin/echo',
-        curl => '/usr/bin/curl',
+        cat      => '/bin/cat',
+        test     => '/usr/bin/test',
+        echo     => '/bin/echo',
+        curl     => '/usr/bin/curl',
+        ifconfig => '/sbin/ifconfig',
     } },
     handles => {
         set_command => 'set',
